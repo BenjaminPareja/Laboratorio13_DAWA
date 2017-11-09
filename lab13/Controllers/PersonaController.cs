3d517536c90f765bb3f675303ff06c818e7de219
Brian pareja <brian.pareja@tecsup.edu.pe>
2017-11-09 01:13:59 -0500

    laboratorio completado

--- lab13/Controllers/PersonaController.cs
+++ lab13/Controllers/PersonaController.cs
@@ -21,35 +21,35 @@ namespace lab13.Controllers
             personas.Add(new Persona
             {
                 PersonaID = 1,
-                Nombre = "Juan",
-                Apellido = "Perez",
+                Nombre = "juan",
+                Apellido = "perez",
                 Direccion = "Av. Evergreen 1234",
                 FechaNac = Convert.ToDateTime("1997-11-07"),
                 Email = "juan@mail.com"
             });
             personas.Add(new Persona
             {
-                PersonaID = 1,
-                Nombre = "Juan",
-                Apellido = "Perez",
+                PersonaID = 2,
+                Nombre = "mario",
+                Apellido = "salluca",
                 Direccion = "Av. Evergreen 1234",
                 FechaNac = Convert.ToDateTime("1997-11-07"),
                 Email = "juan@mail.com"
             });
             personas.Add(new Persona
             {
-                PersonaID = 1,
-                Nombre = "Juan",
-                Apellido = "Perez",
+                PersonaID = 3,
+                Nombre = "benjamin",
+                Apellido = "pareja",
                 Direccion = "Av. Evergreen 1234",
                 FechaNac = Convert.ToDateTime("1997-11-07"),
                 Email = "juan@mail.com"
             });
             personas.Add(new Persona
             {
-                PersonaID = 1,
-                Nombre = "Juan",
-                Apellido = "Perez",
+                PersonaID = 4,
+                Nombre = "jaime",
+                Apellido = "bravo",
                 Direccion = "Av. Evergreen 1234",
                 FechaNac = Convert.ToDateTime("1997-11-07"),
                 Email = "juan@mail.com"
@@ -62,41 +62,86 @@ namespace lab13.Controllers
             personas.Add(new Persona
             {
                 PersonaID = 1,
-                Nombre = "Juan",
-                Apellido = "Perez",
+                Nombre = "juan",
+                Apellido = "perez",
                 Direccion = "Av. Evergreen 1234",
                 FechaNac = Convert.ToDateTime("1997-11-07"),
                 Email = "juan@mail.com"
             });
             personas.Add(new Persona
             {
-                PersonaID = 1,
-                Nombre = "Juan",
-                Apellido = "Perez",
+                PersonaID = 2,
+                Nombre = "mario",
+                Apellido = "salluca",
                 Direccion = "Av. Evergreen 1234",
                 FechaNac = Convert.ToDateTime("1997-11-07"),
                 Email = "juan@mail.com"
             });
             personas.Add(new Persona
             {
-                PersonaID = 1,
-                Nombre = "Juan",
-                Apellido = "Perez",
+                PersonaID = 3,
+                Nombre = "benjamin",
+                Apellido = "pareja",
                 Direccion = "Av. Evergreen 1234",
                 FechaNac = Convert.ToDateTime("1997-11-07"),
                 Email = "juan@mail.com"
             });
             personas.Add(new Persona
             {
-                PersonaID = 1,
-                Nombre = "Juan",
-                Apellido = "Perez",
+                PersonaID = 4,
+                Nombre = "jaime",
+                Apellido = "bravo",
                 Direccion = "Av. Evergreen 1234",
                 FechaNac = Convert.ToDateTime("1997-11-07"),
                 Email = "juan@mail.com"
             });
             Persona persona = (from p in personas
                                where p.PersonaID == id
+                               select p).FirstOrDefault();
+            return View(persona);
+        }
+        public ActionResult Buscar(string Texto)
+        {
+            List<Persona> personas = new List<Persona>();
+            personas.Add(new Persona
+            {
+                PersonaID = 1,
+                Nombre = "juan",
+                Apellido = "perez",
+                Direccion = "Av. Evergreen 1234",
+                FechaNac = Convert.ToDateTime("1997-11-07"),
+                Email = "juan@mail.com"
+            });
+            personas.Add(new Persona
+            {
+                PersonaID = 2,
+                Nombre = "mario",
+                Apellido = "salluca",
+                Direccion = "Av. Evergreen 1234",
+                FechaNac = Convert.ToDateTime("1997-11-07"),
+                Email = "juan@mail.com"
+            });
+            personas.Add(new Persona
+            {
+                PersonaID = 3,
+                Nombre = "benjamin",
+                Apellido = "pareja",
+                Direccion = "Av. Evergreen 1234",
+                FechaNac = Convert.ToDateTime("1997-11-07"),
+                Email = "juan@mail.com"
+            });
+            personas.Add(new Persona
+            {
+                PersonaID = 4,
+                Nombre = "jaime",
+                Apellido = "bravo",
+                Direccion = "Av. Evergreen 1234",
+                FechaNac = Convert.ToDateTime("1997-11-07"),
+                Email = "juan@mail.com"
+            });
+            //Console.WriteLine(texto);
+            Persona persona = (from p in personas
+                               where p.Nombre.Contains(Texto) ||  p.Apellido.Contains(Texto)
                                select p).FirstOrDefault();
             return View(persona);
         }
